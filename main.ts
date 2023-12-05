8160117ef7b48c5e9ffd68c2aef83384abf6c4e4
ezhov-da <ezhdenis@yandex.ru>
2023-12-05 23:13:06 +0300

    feat: plugin created

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import {App, Editor, MarkdownFileInfo, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 interface MyPluginSettings {
-	mySetting: string;
+	regexp: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	regexp: '`tt .*`'
+}
+
+interface TimeTrackerTemplateInfo {
+	id: string;
+	time: number;
+	text: string;
 }
 
 export default class MyPlugin extends Plugin {
@@ -15,67 +19,61 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+		const plugin = this;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		const calculateMinutes = function (text: string) {
+			const infos = plugin.getTimeTrackerTemplateInfo(text)
+			const sum = infos.reduce((accumulator, object) => {
+				return accumulator + object.time;
+			}, 0)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			return sum
+		}
+
+		const setMinutesTuStatusBar = function (text: string) {
+			const sum = calculateMinutes(text)
+
+			statusBarItemEl.setText(`Minutes on file: '${sum}'`);
+		}
+
+		statusBarItemEl.onClickEvent((ev) => {
+			const value = this.app.workspace.activeEditor?.editor?.getValue()
+			if (value) {
+				const info = this.getTimeTrackerTemplateInfo(value);
+				const text = this.getTimeTrackerTemplate(info);
+
+				new SampleModal(this.app, text).open();
+			}
+		})
+
+		const onFileOpenEvent = () => {
+			if (this.app.workspace.activeEditor) {
+				const text = this.app.workspace.activeEditor?.editor?.getValue()
+				if (text) {
+					setMinutesTuStatusBar(text)
 				}
 			}
-		});
+		};
+
+		const onEditorChangeEvent = (editor: Editor, info: MarkdownView | MarkdownFileInfo) => {
+			if (info instanceof MarkdownView) {
+				const text = info.editor.getValue()
+				setMinutesTuStatusBar(text)
+			}
+		}
+
+		this.registerEvent(
+			this.app.workspace.on('file-open', onFileOpenEvent)
+		);
+
+		this.registerEvent(
+			this.app.workspace.on('editor-change', onEditorChangeEvent)
+		);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -89,21 +87,69 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	getTimeTrackerTemplateInfo(inputText: string): Array<TimeTrackerTemplateInfo> {
+		const regex = new RegExp(this.settings.regexp, "g");
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/matchAll
+		const collection = [...inputText.matchAll(regex)].flat();
+
+		return collection.map((v) => {
+			const array = v.split(" ")
+			const jiraId = array[1]
+			const minutes = array[2]
+
+			let textTemp = array.slice(3, array.length).join(" ")
+			textTemp = textTemp.substring(0, textTemp.length - 1)
+			let text = textTemp
+
+			const timeTrackerTemplateInfo: TimeTrackerTemplateInfo = {
+				id: jiraId,
+				time: parseInt(minutes),
+				text: text,
+			}
+
+			return timeTrackerTemplateInfo
+		}) ?? []
+	}
+
+	getTimeTrackerTemplate(info: Array<TimeTrackerTemplateInfo>): string {
+		return info
+			.map((value) => {
+				return `${value.id}__${value.time}_${value.text}`
+			})
+			.join("\n")
+	}
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
+	private readonly text: string;
+
+	constructor(app: App, text: string) {
 		super(app);
+		this.text = text;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+
+		const div = contentEl.createEl("div")
+		div.createEl("pre", {text: this.text})
+
+		div.createEl("button", {text: 'Copy to clipboard',}).onClickEvent(() => {
+			this.copyPathToClipboard(this.text)
+		})
 	}
+
 
 	onClose() {
 		const {contentEl} = this;
 		contentEl.empty();
+	}
+
+	copyPathToClipboard(text: string) {
+		navigator.clipboard.writeText(text).then(() => {
+			new Notice("Template copied to clipboard");
+		});
 	}
 }
 
@@ -121,13 +167,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Search regexp')
+			.setDesc('Search regexp')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Regexp')
+				.setValue(this.plugin.settings.regexp)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.regexp = value;
 					await this.plugin.saveSettings();
 				}));
 	}
